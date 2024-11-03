12d6c44074943421b77cf3eaaa140517e1971751
sananvh <sananvh@gmail.com>
2024-11-03 22:04:51 +0000

    The task five executed

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - Check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
-	char str[] = "Look up!\n";
-	char *ptr;
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
 
-	ptr = string_toupper(str);
-	printf("%s", ptr);
-	printf("%s", str);
+	t = _strstr(s, f);
+	printf("%s\n", t);
 	return (0);
 }
 
