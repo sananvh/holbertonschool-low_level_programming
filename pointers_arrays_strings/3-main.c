87d87e790b30939a2f0e4a0ca643ec11c35bf42d
sananvh <sananvh@gmail.com>
2024-11-03 21:59:50 +0000

    The task four executed

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - Check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
+	char *s = "hello, world";
+	char *f = "oleh";
+	unsigned int n;
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
+	n = _strspn(s, f);
+	printf("%u\n", n);
 	return (0);
 }
 
