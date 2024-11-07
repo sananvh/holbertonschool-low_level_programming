d4fd7477bc9e4fe6a4be3cbf0759df2160198180
sananvh <sananvh@gmail.com>
2024-11-07 11:12:25 +0000

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments it receives, one per line.
+ * @argc: Argument count (number of command-line arguments)
+ * @argv: Argument vector (array of strings containing program name and
+ * arguments)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
