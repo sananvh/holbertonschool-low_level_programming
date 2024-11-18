5dbc59759e74e4a09bbb1fe42b6a7fee08fd59ea
sananvh <sananvh@gmail.com>
2024-11-18 14:44:37 +0000

    Betty style applied to file

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,18 +10,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int i;
+	char *array;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    array = malloc(size * sizeof(char));
-    if (array == NULL)
-        return (NULL);
+	/* Allocate memory for the array */
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
 
-    for (i = 0; i < size; i++)
-        array[i] = c;
+	/* Initialize each element with the character c */
+	for (i = 0; i < size; i++)
+		array[i] = c;
 
-    return (array);
+	return (array);
 }
+
