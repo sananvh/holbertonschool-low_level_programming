70c5962c4bf4f78184f87816678d3003df162cb4
sananvh <sananvh@gmail.com>
2024-11-18 15:19:37 +0000

    New dog file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include "main.h"
-
 /**
  * struct dog - A new type describing a dog
  * @name: The name of the dog (a string)
  * @age: The age of the dog (a float)
  * @owner: The owner of the dog (a string)
  *
- * Description: Defines a dog with a name, age, and owner
+ * Description: Defines a dog with a name, age, and owner.
  */
 struct dog
 {
@@ -19,3 +17,4 @@ struct dog
 };
 
 #endif /* DOG_H */
+
