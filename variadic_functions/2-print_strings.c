0bcaee5939680077ba470ebbe55a789ffe5ac046
sananvh <sananvh@gmail.com>
2024-11-21 10:40:33 +0000

    fila

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdarg.h> /* Include this header for va_start, va_arg, va_end */
+#include <stdarg.h> /* Required for va_start, va_arg, va_end */
 
 /**
  * print_strings - Prints strings, followed by a new line.
@@ -9,26 +9,26 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    char *str;
+	va_list args;
+	unsigned int i;
+	char *str;
 
-    va_start(args, n); /* Initialize the argument list */
+	va_start(args, n); /* Initialize the argument list */
 
-    for (i = 0; i < n; i++)
-    {
-        str = va_arg(args, char *); /* Retrieve the next string */
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *); /* Retrieve the next string */
 
-        if (str == NULL)
-            printf("(nil)");
-        else
-            printf("%s", str);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 
-        if (separator != NULL && i < n - 1)
-            printf("%s", separator);
-    }
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
 
-    va_end(args); /* Clean up the argument list */
-    printf("\n");
+	va_end(args); /* Clean up the argument list */
+	printf("\n");
 }
 
